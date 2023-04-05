35884a00934976209a8dace6800d9bf6a82eac66
izzataa <izzataa@code.edu.az>
2023-04-06 03:47:49 +0400

    Proje dosyası ekle.

---
+++ sasaas/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+string surname = "Abdullayev";
+string result = CustomSubstring(surname, 0,10);
+Console.WriteLine(result); 
+
+static string CustomSubstring(string data, int startIndex, int endIndex)
+{
+    try
+    {
+        string substring = "";
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            substring += data[i];
+        }
+        return substring;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Xəta baş verdi: " + ex.Message);
+        return "";
+    }
+}
